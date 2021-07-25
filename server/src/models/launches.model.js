a8a1d10468034d018489cbb7f001b8a30d35abab
Igor Znamesnky <iznamensky@guardanthealth.com>
2021-07-25 12:50:25 +0300

    110

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,7 +5,7 @@ const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Explorer 1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customer: [
         'ZTM', 'NASA'
     ],
@@ -16,6 +16,10 @@ const launch = {
 let latestFlightNumber = launch.flightNumber;
 
 launches.set(launch.flightNumber, launch);
+
+function existLaunchWithId(launchId) {
+    return launch.has(launchId);
+};
 
 function getAllLaunches() {
     return Array.from(launches.values())
@@ -29,11 +33,17 @@ function addNewLaunch(launch) {
             success: true,
             upcoming: true,
             customer: ['Zero to Mastery', 'NASA'],
-            fightNumber: latestFlightNumber,
+            flightNumber: latestFlightNumber,
     }))
 };
+
+function abortLaunchById(launchId) {
+
+}
 
 module.exports = {
     getAllLaunches,
     addNewLaunch,
+    existLaunchWithId,
+    abortLaunchById,
 };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model')
+const { getAllLaunches, addNewLaunch, existLaunchWithId } = require('../../models/launches.model')
 
 // Any function that working with http response or request
 // will start with 'http'
@@ -10,12 +10,35 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
+
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+        return res.status(400).json({
+            error: 'Missing required launch property'
+        })
+    }
+
     launch.launchDate = new Date(launch.launchDate)
+    if (isNaN(launch.launchDate)){
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        })
+    }
     addNewLaunch(launch);
     return res.status(201).json(launch)
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = req.params.id
+
+    if (!existLaunchWithId(launchId)){
+        return res.status(400).json({
+            error: "Launch does not exist"
+        })
+    }
+}
+
 module.exports = {
     httpGetAllLaunches,
-    httpAddNewLaunch
+    httpAddNewLaunch,
+    httpAbortLaunch,
 }
