48a5493e2db4235258af2641f58d04ef8d43a584
Igor Znamesnky <iznamensky@guardanthealth.com>
2021-07-27 12:49:06 +0300

    117

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,15 +11,58 @@ describe('Test GET /launches', () => {
 })
 
 describe('Test POST /launches', () => {
-    test('200 as response', () => {
+    const completeLaunchDate = {
+                mission: 'Deep V ANAL',
+                rocket: 'XXX 22 CM Black',
+                target: 'Mega PIZDA 4X',
+                launchDate: 'January 4, 2023'
+            };
+
+    const launchDataWithoutDate = {
+                mission: 'Deep V ANAL',
+                rocket: 'XXX 22 CM Black',
+                target: 'Mega PIZDA 4X',
+            };
+
+    test('201 as response and created', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(completeLaunchDate)
+            .expect('Content-Type', /json/)
+            .expect(201)
+
+        const requestDate = new Date(completeLaunchDate.launchDate).valueOf(); // to get numerical
+        const responseDate = new Date(response.body.launchDate).valueOf();
+        expect(responseDate).toBe(requestDate);
+
+        expect(response.body).toMatchObject(launchDataWithoutDate);
+    })
+
+    test('It should catch missing required properties', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchDataWithoutDate)
+            .expect('Content-Type', /json/)
+            .expect(400)
+
+        expect(response.body).toStrictEqual({
+            error: 'Missing required launch property'
+        });
 
     })
 
-    test('It should catch missing required properties', () => {
+    test('It should catch invalid date', async () => {
+        completeLaunchDate.launchDate = 'baz'
 
-    })
+        const response = await request(app)
+            .post('/launches')
+            .send(completeLaunchDate)
+            .expect('Content-Type', /json/)
+            .expect(400)
 
-    test('It should catch invalid date', () => {
+        expect(response.body).toStrictEqual({
+            error: 'Invalid launch date'
+        });
 
     })
 })
